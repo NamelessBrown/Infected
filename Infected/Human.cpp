06c16f525e12e06a25bbb78087a00fb06e02ce69
Nameless <tyrebrown873@gmail.com>
2020-03-13 18:05:23 -0400

    Human names of people! Randomly generated

--- Infected/Human.cpp
+++ Infected/Human.cpp
@@ -1,1 +1,11 @@
 #include "Human.h"
+#include <iostream>
+
+Human::Human()
+	:mIsInfected(false), mRd(), mMt(mRd()), mAgeDistribution(0, 100), mNameDistribution(0, m_names.size() - 1)
+{
+	mAge = mAgeDistribution(mMt);
+
+	mName = m_names.at(mNameDistribution(mMt));
+
+}
--- Infected/Human.h
+++ Infected/Human.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Disease.h"
+#include <vector>
 
 class Human
 {
@@ -11,5 +12,18 @@ private:
 	std::string mName;
 	bool mIsInfected;
 	Disease mDisease;
+
+	std::vector<std::string> m_names{
+		"Joe", "Bob", "Frank", "Billy", "Adien", "NamelessBrown", "Mia", "Madison", "Chole", "Orion", "Marlon", "Chris", "Reggie",
+		"CodePrime8", "TheFatRat", "Laura_Brehm", "Jonathon_Hills", "Mr.Butts", "Physical_Gamerz", "Mr.Miss", "Ariana", "Squidward",
+		"Spongeboob", "Patrick", "Sandy", "Miss.Puff", "Wille", "Cameron", "Jesus", "Buddha", "Berleezy", "Infected_monsta", "Mr.Horrigan",
+		"The_Lone_Wanderer", "Lily_Madwhip", "Kevin_Macleod", "ChiliTomatoNoodle", "The_toe_bro", "Varyana", "Hugo_One", "Don", "Mr.Smith"
+	};
+
+	std::random_device mRd;
+	std::mt19937 mMt;
+	std::uniform_int_distribution<int> mAgeDistribution;
+	std::uniform_int_distribution<int> mNameDistribution;
+
 };
 
